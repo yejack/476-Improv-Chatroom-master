8e2c73252e0382dbd979f28fb4cf956821f832f3
yejack <yeshuchuan000@gmail.com>
2017-12-04 21:24:31 -0500

    12

--- improv_app/routes.py
+++ improv_app/routes.py
@@ -19,7 +19,7 @@ app.secret_key = 'development-key'
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index1.html")
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
 	form = SignupForm()
